7f3f53c71b61090e5b9fb32f6a1b9bdd737d9cdb
djalves424 <djalves424@gmail.com>
2024-01-30 22:48:43 -0300

    Update the properties file

--- user/src/main/java/com/ms/user/producers/UserProducer.java
+++ user/src/main/java/com/ms/user/producers/UserProducer.java
@@ -23,7 +23,7 @@ public class UserProducer {
         emailDto.setUserId(userModel.getUserId());
         emailDto.setEmailTo(userModel.getEmail());
         emailDto.setSubject("Cadastro realizado com sucesso!");
-        emailDto.setText(userModel.getName() + ", Seja bem vindo(a)! \nAfradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma");
+        emailDto.setText(userModel.getName() + ", Seja bem vindo(a)! \nAgradecemos o seu cadastro, aproveite agora todos os recursos da nossa plataforma");
 
         rabbitTemplate.convertAndSend("", routingKey, emailDto);
     }
